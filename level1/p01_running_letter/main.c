fe272f44e631434ab23defb93467044decd755ff
duang <3830527488@qq.com>
2026-09-22 20:58:38 +0800

    feat：完成主体代码

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main() {
-    printf("hello world!\n");
+    char text[] = "hello world!";
+
+    while (1) {
+        for (int n = 0; n < 20; n++) {
+            system("clear");
+            for (int i = 0; i < n; i++) {
+                printf(" ");
+
+            }
+            printf("%s\n", text);
+            usleep(100000);
+        }
+
+        for (int n = 18; n >= 1; n--) {
+            system("clear");
+            for (int i = 0; i < n; i++) {
+                printf(" ");
+
+            }
+            printf("%s\n", text);
+            usleep(100000);
+        }
+    }
+
+
+
     return 0;
 }
